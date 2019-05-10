639bf5bf4d64fadb9dd77600805a97b0069da1fd
Piotr Antonowicz <pantonowicz@gmail.com>
2019-05-10 11:50:54 +0100

    routes & controller created for Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ
+  get 'tasks', to: 'tasks#index'
+
+  # CREATE
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # EDIT
+  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  #DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
